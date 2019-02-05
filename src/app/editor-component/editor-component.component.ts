8f9c79d531500948c3400553b49210b8fa466f56
shahyash62 <shahyash62@gmail.com>
2019-02-05 17:27:31 -0500

    -

---
+++ src/app/editor-component/editor-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-editor-component',
+  templateUrl: './editor-component.component.html',
+  styleUrls: ['./editor-component.component.sass']
+})
+export class EditorComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
